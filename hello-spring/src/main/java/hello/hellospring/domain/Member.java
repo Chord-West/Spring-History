2975a26d453e5f72d99d86cf0fb0269217e50d20
Chord-West <gustj2005@naver.com>
2022-09-06 19:02:35 +0900

    study spring JPA

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,16 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // jpa 가 관리하는 Entity
 public class Member {
 
+
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)  // DB가 알아서 PK로 명시
     private Long id;
     private String name;
 
